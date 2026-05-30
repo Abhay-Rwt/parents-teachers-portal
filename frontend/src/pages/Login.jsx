60d8d0a2a4c3021dd55907cb4f306682e7b8248b
Abhay-Rwt <abhithrckstr90@gmail.com>
2026-05-30 14:36:30 +0530

    changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import '../index.css';
 
 const Login = () => {
     const [email, setEmail] = useState('');
